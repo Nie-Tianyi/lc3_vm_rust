edfc7f3fb16261f60eb5672dda2edc989f4cf490
nty-mac <1355705558@qq..com>
2024-12-04 00:35:19 +0800

    feat: add common instructions implementation

--- src/lc3_vm.rs
+++ src/lc3_vm.rs
@@ -1,6 +1,8 @@
 mod config;
-mod operand;
+mod util;
+
 use config::*;
+use util::*;
 
 struct LC3VM {
     memory: [u16; MEMORY_SIZE], // total memory size
@@ -13,9 +15,38 @@ impl LC3VM {
         let memory = [0; MEMORY_SIZE];
         let mut reg = [0; REG_COUNT];
         reg[R_PC] = PC_START;
+        reg[R_COND] = FL_ZRO;
 
         LC3VM { memory, reg, debug }
     }
+
+    pub fn execute(&mut self) {
+        loop {
+            let ins = self.read_address(self.reg[R_PC]);
+            self.inc_pc(1);
+            let opcode = ins >> 12;
+
+            match opcode {
+                OP_ADD => self.add(ins),
+                OP_LD => self.ld(ins),
+                OP_ST => self.st(ins),
+                OP_JSR => self.str(ins),
+                OP_AND => self.and(ins),
+                OP_LDR => self.ldr(ins),
+                OP_STR => self.str(ins),
+                OP_NOT => self.not(ins),
+                OP_LDI => self.ldi(ins),
+                OP_STI => self.sti(ins),
+                OP_JMP => self.jmp(ins),
+                OP_LEA => self.lea(ins),
+                OP_TRAP => self.trap(ins),
+                OP_RES | OP_RTI => panic!("Abandoned Instructions"),
+                _ => panic!("Unknown Instruction {opcode}")
+            }
+        }
+    }
+}
+impl LC3VM {
     #[inline]
     fn is_debug(&self) -> bool {
         self.debug
@@ -45,8 +76,8 @@ impl LC3VM {
         self.reg[R_PC] = val
     }
     #[inline]
-    fn inc_pc(&mut self) {
-        self.reg[R_PC] += 1;
+    fn inc_pc(&mut self, n: u16) {
+        self.reg[R_PC] += n;
     }
     #[inline]
     fn cond(&self) -> u16 {
@@ -86,6 +117,131 @@ impl LC3VM {
     }
 }
 
+impl LC3VM {
+    fn add(&mut self, ins: u16) {
+        // & P_3 means only retain last 3 bits
+        let r0 = (ins >> 9) & P_3; // destination register
+        let r1 = (ins >> 6) & P_3;
+        let imm_flag = (ins >> 6) & P_1;
+
+        if imm_flag == 1 {
+            let imm5 = sign_extend(ins & P_5, 5);
+            self.write_reg(r0, self.register(r1) + imm5);
+        } else {
+            let r2 = ins & P_3;
+            self.write_reg(r0, self.register(r1) + self.register(r2))
+        }
+
+        self.update_flag(r0);
+    }
+    // load indirect
+    fn ldi(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let pc_offset = sign_extend(ins & P_9, 9);
+
+        let address = self.read_address(self.pc() + pc_offset);
+
+        self.write_reg(r0, self.read_address(address));
+    }
+    // bitwise and operation
+    fn and(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let r1 = (ins >> 6) & P_3;
+        let imm_flag = (ins >> 5) & P_1;
+
+        if imm_flag == 1 {
+            let imm5 = sign_extend(ins & P_5, 5);
+            self.write_reg(r0, self.register(r1) & imm5)
+        } else {
+            let r2 = ins & P_3;
+            self.write_reg(r0, self.register(r1) & self.register(r2))
+        }
+
+        self.update_flag(r0);
+    }
+    // bitwise not operation
+    fn not(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let r1 = (ins >> 6) & P_3;
+
+        self.write_reg(r0, !self.register(r1));
+        self.update_flag(r0);
+    }
+    // branch
+    fn br(&mut self, ins: u16) {
+        let pc_offset = sign_extend(ins & P_9, 9);
+        let cond_flag = (ins >> 9) & P_3;
+
+        if cond_flag & self.cond() != 0 {
+            self.inc_pc(pc_offset);
+        }
+    }
+    // jump
+    fn jmp(&mut self, ins: u16) {
+        let r1 = (ins >> 6) & P_3;
+        self.set_pc(self.register(r1));
+    }
+    // jump register
+    fn jsr(&mut self, ins: u16) {
+        let long_flag = (ins >> 11) & P_1;
+        self.write_reg(R7 as u16, self.pc());
+
+        if long_flag == 1 {
+            let long_pc_offset = sign_extend(ins & P_11, 11);
+            self.inc_pc(long_pc_offset);
+        } else {
+            let r1 = (ins >> 6) & P_3;
+            self.set_pc(self.register(r1));
+        }
+    }
+    // load
+    fn ld(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let pc_offset = sign_extend(ins & P_9, 9);
+        self.write_reg(r0, self.read_address(self.pc() + pc_offset));
+        self.update_flag(r0);
+    }
+    // load register
+    fn ldr(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let r1 = (ins >> 6) & P_3;
+
+        let offset = sign_extend(ins & P_6, 6);
+        self.write_reg(r0, self.read_address(self.register(r1) + offset));
+        self.update_flag(r0);
+    }
+    // load effective address
+    fn lea(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let pc_offset = sign_extend(ins & P_9, 9);
+        self.write_reg(r0, self.pc() + pc_offset);
+        self.update_flag(r0);
+    }
+    // store
+    fn st(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let pc_offset = sign_extend(ins & P_9, 9);
+        self.write_address(self.pc() + pc_offset, self.register(r0));
+    }
+    // store indirect
+    fn sti(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let pc_offset = sign_extend(ins & P_9, 9);
+        self.write_address(self.read_address(self.pc() + pc_offset), self.register(r0));
+    }
+
+    fn str(&mut self, ins: u16) {
+        let r0 = (ins >> 9) & P_3;
+        let r1 = (ins >> 6) & P_3;
+        let offset = sign_extend(ins & P_6, 6);
+        self.write_address(self.register(r1) + offset, self.register(r0));
+    }
+
+    fn trap(&mut self, ins: u16) {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -95,3 +251,4 @@ mod tests {
         let vm = LC3VM::new(true);
     }
 }
+
--- src/lc3_vm/util.rs
+++ src/lc3_vm/util.rs
@@ -1,11 +1,8 @@
 /// extend a 5-bit signed integer to 16-bit signed integer
 #[inline]
-fn sign_extend(mut num: u16, bits: usize) -> u16 {
+pub(crate) fn sign_extend(mut num: u16, bits: usize) -> u16 {
     if num >> (bits - 1) != 0 {
         num |= 0xFFFF << bits
     }
     num
 }
-
-
-mod add;
