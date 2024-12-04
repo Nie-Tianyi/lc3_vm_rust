9b498d1e59a47b6455147d68513e07b4280414d7
nty-mac <1355705558@qq..com>
2024-12-04 22:51:56 +0800

    feat: implement trap

--- src/lc3_vm.rs
+++ src/lc3_vm.rs
@@ -1,6 +1,7 @@
 mod config;
 mod util;
 
+use std::io::{Read, Write};
 use config::*;
 use util::*;
 
@@ -41,7 +42,7 @@ impl LC3VM {
                 OP_LEA => self.lea(ins),
                 OP_TRAP => self.trap(ins),
                 OP_RES | OP_RTI => panic!("Abandoned Instructions"),
-                _ => panic!("Unknown Instruction {opcode}")
+                _ => panic!("Unknown Instruction {opcode}"),
             }
         }
     }
@@ -51,6 +52,7 @@ impl LC3VM {
     fn is_debug(&self) -> bool {
         self.debug
     }
+
     #[inline]
     fn register(&self, reg: u16) -> u16 {
         let reg = reg as usize;
@@ -59,6 +61,7 @@ impl LC3VM {
         }
         self.reg[reg]
     }
+
     #[inline]
     fn write_reg(&mut self, reg: u16, val: u16) {
         let reg = reg as usize;
@@ -67,38 +70,35 @@ impl LC3VM {
         }
         self.reg[reg] = val;
     }
+
     #[inline]
     fn pc(&self) -> u16 {
         self.reg[R_PC]
     }
+
     #[inline]
     fn set_pc(&mut self, val: u16) {
         self.reg[R_PC] = val
     }
+
     #[inline]
     fn inc_pc(&mut self, n: u16) {
         self.reg[R_PC] += n;
     }
+
     #[inline]
     fn cond(&self) -> u16 {
         self.reg[R_COND]
     }
+
     #[inline]
     fn read_address(&self, address: u16) -> u16 {
         self.memory[address as usize]
     }
+
     #[inline]
     fn write_address(&mut self, address: u16, val: u16) {
         self.memory[address as usize] = val;
-    }
-
-    #[inline]
-    fn stdin(&self) -> std::io::Stdin {
-        std::io::stdin()
-    }
-    #[inline]
-    fn stdout(&self, content: impl std::fmt::Display) {
-        println!("{content}")
     }
 
     fn update_flag(&mut self, reg: u16) {
@@ -238,7 +238,62 @@ impl LC3VM {
     }
 
     fn trap(&mut self, ins: u16) {
-        unimplemented!()
+        self.write_reg(R7 as u16, self.pc());
+        match ins & P_8 {
+            TRAP_GETC => { // get char
+                let mut buffer = [0;1];
+                std::io::stdin().read_exact(&mut buffer).unwrap();
+                self.write_reg(R0 as u16, buffer[0] as u16);
+                self.update_flag(R0 as u16);
+            }
+            TRAP_OUT => {
+                let c = self.register(R0 as u16) as u8;
+                print!("{}", c as char);
+            }
+            TRAP_PUTS => {
+                let mut index = self.register(R0 as u16);
+                let mut c = self.read_address(index);
+                while c != 0x0 {
+                    print!("{}", c as u8 as char);
+                    index += 1;
+                    c = self.read_address(index);
+                }
+                std::io::stdout().flush().expect("Failed to Flush");
+            }
+            TRAP_IN => {
+                print!("Enter a  character : ");
+                std::io::stdout().flush().expect("failed to flush");
+                let char = std::io::stdin()
+                    .bytes()
+                    .next()
+                    .and_then(|result| result.ok())
+                    .map(|byte| byte as u16)
+                    .unwrap();
+                self.write_reg(R0 as u16, char);
+                self.update_flag(R0 as u16);
+            }
+            TRAP_PUTSP => {
+                let mut index = self.register(R0 as u16);
+                let mut c = self.read_address(index);
+                while c != 0x0 {
+                    let c1 = ((c & 0xFF) as u8) as char;
+                    print!("{}", c1);
+                    let c2 = ((c >> 8) as u8) as char;
+                    if c2 != '\0' {
+                        print!("{}", c2);
+                    }
+                    index += 1;
+                    c = self.read_address(index);
+                }
+                std::io::stdout().flush().expect("Fail to Flush");
+            }
+            TRAP_HALT => {
+                println!("HALT detected");
+                std::io::stdout().flush().expect("Fail to Flush");
+                std::process::exit(1);
+            }
+            _ => panic!("Unknown Trap Code"),
+        }
     }
 }
 
@@ -251,4 +306,3 @@ mod tests {
         let vm = LC3VM::new(true);
     }
 }
-
